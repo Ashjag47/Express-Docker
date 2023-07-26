f4e1feb27d8c062864b70b8ae0bc4b79de88bfd6
Ashjag47 <ashjagmk@gmail.com>
2023-07-26 18:06:13 +0530

    Feat: connect mongoDb with express server with health check in compose file and retry logic in app.

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_PASSWORD, MONGO_USER, MONGO_IP, MONGO_PORT } = require('./config/config');
+
 const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+const connnectWithRetry = () => {
+    mongoose
+        .connect(mongoURL, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+        })
+        .then(() => console.log('MongoDB Connected'))
+        .catch(err => {
+            console.log(err);
+            setTimeout(connnectWithRetry, 5000);
+        });
+};
+
+connnectWithRetry();
+
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`));
 
